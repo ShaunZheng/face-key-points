d2559fb22ff01e07f5b1d0ccf104bf957cfdbbe8
taojinglong <craftsman.mote@gmail.com>
2017-12-08 11:05:26 +0800

    update

--- face_key_points.py
+++ face_key_points.py
@@ -4,12 +4,13 @@ import cv2
 import dlib
 import numpy as np 
     
+
 cap = cv2.VideoCapture(0)
 cap.isOpened()
 
 def ket_points(img):
     # 人脸关键点检测
-    PREDICTOR_PATH = "../some-big-model-file/shape_predictor_68_face_landmarks.dat"
+    PREDICTOR_PATH = "../../some-big-model-file/shape_predictor_68_face_landmarks.dat"
     
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(PREDICTOR_PATH)
@@ -25,13 +26,12 @@ def ket_points(img):
             cv2.circle(img,pos,2,(255,0,0),-1)
     return img
 
-
 def frontalface(img):
     # 人脸检测
     face_cascade = cv2.CascadeClassifier(r'/opt/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
 
     faces = face_cascade.detectMultiScale(
-        gray,scaleFactor = 1.15,minNeighbors = 5,minSize = (5,5),flags = cv2.CV_HAAR_SCALE_IMAGE)
+        gray,scaleFactor = 1.15,minNeighbors = 5,minSize = (5,5))
 
     for(x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+w),(0,255,0),2)
@@ -43,7 +43,7 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face_key = ket_points(gray)
-    # face_detector = frontalface(gray)
+    face_detector = frontalface(gray)
     cv2.imshow('frame',face_key)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
